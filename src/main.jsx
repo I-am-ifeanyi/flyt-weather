6348a5fd1c7843d2aa0dd021fc67b2766522293d
Ifeanyi <theonyekagroup@gmail.com>
2023-04-21 11:51:20 +0100

    Bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { StateManagement } from "./StateManagement";
 import { QueryClientProvider, QueryClient } from "react-query";
 
@@ -11,11 +11,11 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <StateManagement>
           <App />
         </StateManagement>
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>
   </QueryClientProvider>
 );
