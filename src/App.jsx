72bd03c445fe469c7431813a205b3328d50a957a
Ifeanyi Onyeka <theonyekagroup@gmail.com>
2023-06-12 14:56:53 +0100

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,11 @@ import ProtectedRoute from "./pages/ProtectedRoute";
 import ProtectedRoute2 from "./pages/ProtectedRoute2";
 
 function App() {
-  return (
+  return (<div>
+    <div className="w-full h-screen hidden md:flex items-center justify-center text-3xl text-white bg-[#7F4CD2] font-bold tracking-widest">
+      <h1>Mobile Responsive Only</h1>
+    </div>
+    <div className="md:hidden">
     <Routes>
       <Route index element={<Intro />} />
       <Route path="/login" element={<Login />} />
@@ -39,6 +43,8 @@ function App() {
         element={<ThreeHourlyDetails />}
       />
     </Routes>
+    </div>
+  </div>
   );
 }
 
